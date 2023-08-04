493131c8dde94f618210320bbb2f87ae0c418e1f
Umid <umid-berdiev82@mail.ru>
2023-08-04 17:28:32 +0500

    feat: bootstrap removed and nuxtlab ui added

---
+++ components/Headings/PrimaryHeading.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="primary-heading">
+    <span class="content">
+      <slot />
+    </span>
+    <div class="line" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .primary-heading {
+    display: inline-flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 16px;
+
+    .content {
+      color: #162826;
+      font-family: Roboto Condensed;
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+      text-transform: uppercase;
+    }
+
+    .line {
+      width: 80px;
+      height: 4px;
+      background: #da3832;
+    }
+  }
+</style>

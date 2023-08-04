b3b86778463d70a59284eedf2c69af9ab6062d9f
Umid <umid-berdiev82@mail.ru>
2023-08-04 14:40:03 +0500

    feat: footer in progres

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,13 +2,13 @@
   <div class="container-fluid position-fixed bottom-0">
     <div class="row">
       <div class="col-12 bg-light d-flex justify-content-between">
-        <img src="@/assets/images/Group_1780.png" alt="" />
-        <img src="@/assets/images/Group_1775.png" alt="" />
-        <img src="@/assets/images/Group_1772.png" alt="" />
-        <img src="@/assets/images/Group_1779.png" alt="" />
-        <img src="@/assets/images/Group_1778.png" alt="" />
-        <img src="@/assets/images/Group_1774.png" alt="" />
-        <img src="@/assets/images/Group_1773.png" alt="" />
+        <img src="@/assets/images/Group_1780.svg" alt="Group_1780" />
+        <img src="@/assets/images/Group_1775.svg" alt="Group_1775" />
+        <img src="@/assets/images/Group_1772.svg" alt="Group_1772" />
+        <img src="@/assets/images/Group_1779.svg" alt="Group_1779" />
+        <img src="@/assets/images/Group_1778.svg" alt="Group_1778" />
+        <img src="@/assets/images/Group_1774.svg" alt="Group_1774" />
+        <img src="@/assets/images/Group_1773.svg" alt="Group_1773" />
       </div>
       <div class="col-12"></div>
     </div>
